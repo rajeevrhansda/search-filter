61e2316e03274386d2d6a0a5ce1e94511cb19d89
Rajeev Hansda <rajeevrhansda@gmail.com>
2022-07-31 11:18:36 +0530

    splice

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ import cors from "cors";
 app.use(cors());
 
 app.get("/", (req, res) => {
-    res.json(Users);
+    res.json(Users.splice(0,10));
 });
 
 app.listen(5000, () => console.log("API is working!"));
