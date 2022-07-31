d858be64b27ce4eb867f912da11118279c09f4f1
Rajeev Hansda <rajeevrhansda@gmail.com>
2022-07-31 06:48:14 +0530

    basic style and code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <input
+        class="search"
+        placeholder="Search..."
+        type="text"
+      />
+    <ul class="list">
+      <li class="listItem">John</li>
+      <li class="listItem">Jane</li>
+      <li class="listItem">Mike</li>
+    </ul>
     </div>
   );
 }
