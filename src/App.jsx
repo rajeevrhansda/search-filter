72744cab2a387a86a9e14c078f08efde583591ee
Rajeev Hansda <rajeevrhansda@gmail.com>
2022-07-31 07:42:32 +0530

    filter ||, search prop

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,24 @@ import { Users } from './users'
 
 function App() {
   const [query, setQuery] = useState("");
+  const search = (data) => {
+    return data.filter(
+      (item) =>
+      item.first_name.toLowerCase().includes(query) ||
+      item.last_name.toLowerCase().includes(query) ||
+      item.email.toLowerCase().includes(query)
+    );
+  };
+  
   return (
     <div className="app">
       <input
         className="search"
         placeholder="Search..."
         type="text"
-        onChange={(e)=> setQuery(e.target.value)}
+        onChange={(e) => setQuery(e.target.value)}
       />
-      <Table data={Users}/>
+      <Table data={search(Users)} />
     </div>
   );
 }
