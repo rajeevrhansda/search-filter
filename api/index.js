1b1eee938aef60e28b16e0a4a0051c7681e65cfc
Rajeev Hansda <rajeevrhansda@gmail.com>
2022-07-31 11:27:18 +0530

    fetch data by query

--- api/index.js
+++ api/index.js
@@ -6,11 +6,14 @@ import cors from "cors";
 app.use(cors());
 
 app.get("/", (req, res) => {
-    // const q = req.query.q;
     const {q} = req.query;
-    // http://localhost:5000/?q=asd
-    console.log(q);
-    res.json(Users.splice(0,10));
+
+    const keys = ["first_name", "last_name", "email"];
+    const search = (data)=>{
+        return data.filter((item)=>
+        keys.some((key)=> item[key].toLowerCase().includes(q)))
+    }
+    res.json(search(Users).splice(0,10));
 });
 
 app.listen(5000, () => console.log("API is working!"));
