3281d7ad16141e6f20dd2c3d0fbaa8cfdfedc1a1
Rajeev Hansda <rajeevrhansda@gmail.com>
2022-07-31 11:17:32 +0530

    fetch data server table

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
+import { useEffect } from 'react';
 import { useState } from 'react';
 import './App.css';
 import Table from './Table';
+import axios from 'axios';
 
 function App() {
   const [query, setQuery] = useState("");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async ()=>{
+      const res = await axios.get("http://localhost:5000");
+      setData(res.data);
+    }
+    fetchUsers();
+  },[])
 
   return (
     <div className="app">
@@ -13,7 +24,7 @@ function App() {
         type="text"
         onChange={(e) => setQuery(e.target.value)}
       />
-      <Table data={[]} />
+      <Table data={data} />
     </div>
   );
 }
