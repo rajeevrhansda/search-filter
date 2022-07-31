da6a6658c844cce556f762871d048e88e59d1887
Rajeev Hansda <rajeevrhansda@gmail.com>
2022-07-31 07:49:26 +0530

    key array, some, filter, table, data prop

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,15 @@ import { Users } from './users'
 
 function App() {
   const [query, setQuery] = useState("");
+  const keys = ["first_name", "last_name", "email"];
+
   const search = (data) => {
     return data.filter(
       (item) =>
-      item.first_name.toLowerCase().includes(query) ||
-      item.last_name.toLowerCase().includes(query) ||
-      item.email.toLowerCase().includes(query)
+      keys.some((key)=> item[key].toLowerCase().includes(query))
     );
   };
-  
+
   return (
     <div className="app">
       <input
